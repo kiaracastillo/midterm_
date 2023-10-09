19b8e9bfcf36566b9f2ae9ed864118973d89cec9
alrwest <alrwest@iu.edu>
2023-10-09 09:42:39 -0400

    updating about

--- app/model.js
+++ app/model.js
@@ -2,13 +2,22 @@ const homeRoute = `
 <h1>Home</h1>
 `;
 const aboutRoute = `
-<h1>About</h1>
+<div class = "about-section">
+  <div class = "about-content">
+    <h1>About</h1>
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis laborum ipsum, vel dolorem error dolorum unde praesentium doloremque quibusdam sed harum eaque, perspiciatis aperiam fugiat illo voluptates nulla, eos sequi?</p>
+
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis laborum ipsum, vel dolorem error dolorum unde praesentium doloremque quibusdam sed harum eaque, perspiciatis aperiam fugiat illo voluptates nulla, eos sequi?</p>
+    </div>
+</div>
+
+
 `;
 const productsRoute = `
 <h1>Products</h1>
 `;
 const contactRoute = `
-<h1>contact</h1>
+<h1>Contact</h1>
 
 `;
 
