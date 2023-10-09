74b66e4eea13ab9c84ee5fcb03f9819e27199d09
kiaracastillo <kiaracastillom@gmail.com>
2023-10-09 10:10:32 -0400

    Co-authored-by: alrwest <alrwest@users.noreply.github.com>

--- app/model.js
+++ app/model.js
@@ -1,14 +1,31 @@
 const homeRoute = `
-<h1>Home</h1>
+<div class="home">
+
+<div class="hero">
+<div class="callout">
+<h1>Lorem</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent in feugiat eros. Donec a dictum arcu. </p>
+<button>Lorem</button>
+</div>
+</div>
+
+</div>
 `;
 const aboutRoute = `
-<h1>About</h1>
+<div class = "about-section">
+  <div class = "about-content">
+    <h1>About</h1>
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis laborum ipsum, vel dolorem error dolorum unde praesentium doloremque quibusdam sed harum eaque, perspiciatis aperiam fugiat illo voluptates nulla, eos sequi?</p>
+
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis laborum ipsum, vel dolorem error dolorum unde praesentium doloremque quibusdam sed harum eaque, perspiciatis aperiam fugiat illo voluptates nulla, eos sequi?</p>
+    </div>
+</div>
 `;
 const productsRoute = `
 <h1>Products</h1>
 `;
 const contactRoute = `
-<h1>contact</h1>
+<h1>Contact</h1>
 
 `;
 
